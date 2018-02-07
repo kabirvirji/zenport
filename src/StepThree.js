062d49131075d19c497ae794f98321c970ca6cd6
kabirvirji <kabirvirji@gmail.com>
2018-02-07 18:24:07 -0500

    valid number

--- src/StepThree.js
+++ src/StepThree.js
@@ -43,20 +43,21 @@ class StepThree extends Component {
   }
 
   addItem(event) {
-    // take current meal and current serving from state
-    // add number to total dishes
-    // add it to object in state
-    // check to see if total dishes is enough, if it is change validNumber
-    //const currTotal = this.state.currentServing // 2
     console.log(this.state.currentMeal)
     console.log(this.state.currentServing)
     event.preventDefault();
     this.setState((prevState) => ({
-      totalDishes: prevState.currentServing,
-      meals: {...prevState.meals, [prevState.currentMeal]: prevState.currentServing}
+      totalDishes: +prevState.totalDishes + +this.state.currentServing,
+      meals: {...prevState.meals, [prevState.currentMeal]: prevState.currentServing},
+      validNumber: +prevState.totalDishes + +this.state.currentServing <= 10 && +prevState.totalDishes + +this.state.currentServing >= this.props.previousValues.numberOfGuests ? true : false
     }))
     
     // if dishes enough
+    // if (this.state.totalDishes <= 10 && this.state.totalDishes >= this.props.previousValues.numberOfGuests) {
+    //   this.setState({
+    //     validNumber: true
+    //   })
+    // }
   }
 
   // when add, see if it is valid number
@@ -76,6 +77,10 @@ class StepThree extends Component {
     // }
     // console.log(this.state.meals)
 
+    // {[this.state.meals].map(i => {i, this.state.meals[i]})}
+    // <h2>Ordered: {availableMeals.map(i => <p>{i, this.state.meals.i}</p>)}</h2>
+    // const mealsArr = [this.state.meals].entries()
+    // console.log(mealsArr)
     return (
       <form>
         <select
@@ -92,7 +97,8 @@ class StepThree extends Component {
           onChange={this.handleInputServing} 
         />
         <h3>Please enter between {this.props.previousValues.numberOfGuests} and 10 meals (inclusive)</h3>
-        <h3>Current total meal(s) : {this.state.totalDishes}</h3>
+        <h3>Current total number of meal(s) : {this.state.totalDishes}</h3>
+        
         <button onClick={ this.addItem }>add item</button>
         <button onClick={ this.props.previousStep }>previous</button>
         <button onClick={ this.saveInput } disabled={!this.state.validNumber}>Save and Continue</button>
