5218c5cb64aaa896e3bfccbcbe91d73a082d61c1
kabirvirji <kabirvirji@gmail.com>
2018-02-15 10:39:54 -0500

    tests for 3 done

--- src/__tests__/StepThree.test.js
+++ src/__tests__/StepThree.test.js
@@ -6,8 +6,6 @@ import { shallow, mount, configure } from 'enzyme'
 
 configure({ adapter: new Adapter() });
 
-// check button disabeld based on previousValues and meals addpreviousValues={}
-
 it('correct dropdown', () => {
     const wrapper = shallow(<StepThree previousValues={{restaurant: 'Mc Donalds'}}/>)
     expect(wrapper.containsAllMatchingElements([
@@ -45,11 +43,12 @@ it('delete meal', () => {
     expect(wrapper.state().totalDishes).toEqual(2)
 })
 
-// not enough meals, too many meals
-// it('button should be disabled if there are not enough meals for the guests', () => {
-//     const wrapper = shallow(<StepThree previousValues={{numberOfGuests: 8}}/>)
-//     const input = wrapper.find('select')
-//     input.simulate('change', {target: {name: "restaurant", value: 'default' }})
-//     const Button = wrapper.find('button').at(1)
-//     expect(Button.props().disabled).toEqual(true) 
-// })
+it('button should be disabled if there are not enough meals for the guests', () => {
+    const wrapper = shallow(<StepThree previousValues={{numberOfGuests: 8, restaurant: 'Mc Donalds'}}/>)
+    const select = wrapper.find('select')
+    const input = wrapper.find('input')
+    select.simulate('change', {target: {name: "currentMeal", value: "Cheese Burger" }})
+    input.simulate('change', {target: {name: "currentServing", value: 3 }})
+    const Button = wrapper.find('button').at(3)
+    expect(Button.props().disabled).toEqual(true) 
+})
